4ecb29a9dc3e755a5aded49a8165d5e782f7b4f1
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-08 23:37:12 +0200

    added javascript as responsible for submitting the form and saved the data in variables as numbers

--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ const myForm = document.getElementById('priceCalculator');
 
 const kmInput = document.getElementById('kmInput');
 const ageInput = document.getElementById('ageInput');
+
+//asigned variables
+
+let age;
+let km;
+
+//read the input data before submiting the form
+
+myForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    //transform input data into numbers
+    age = Number(ageInput.value);
+    km = Number(kmInput.value);
+    
+    //clear input areas
+    kmInput.value = '' ;
+    ageInput.value = '' ;
+});
