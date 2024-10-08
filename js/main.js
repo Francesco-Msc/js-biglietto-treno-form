b08748e555f5ccf057fccc3a833de79ad30de7e0
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-08 23:32:42 +0200

    retrived form and input areas from dom into js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//get form from dom
+const myForm = document.getElementById('priceCalculator');
+
+//user input area
+
+const kmInput = document.getElementById('kmInput');
+const ageInput = document.getElementById('ageInput');
