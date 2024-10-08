d154d36089eed67303c0d7c3fc51c934673d18a8
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-10-08 23:54:16 +0200

    added price calculations and print in console log

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+//known prices and discounts
+const kmPrice = 0.21;
+const underAge = 18;
+const overAge = 65;
+const underDiscount = 20;
+const overDiscount = 40;
+
 //get form from dom
 const myForm = document.getElementById('priceCalculator');
 
@@ -10,6 +17,7 @@ const ageInput = document.getElementById('ageInput');
 
 let age;
 let km;
+let price;
 
 //read the input data before submiting the form
 
@@ -19,6 +27,25 @@ myForm.addEventListener('submit', function(event){
     //transform input data into numbers
     age = Number(ageInput.value);
     km = Number(kmInput.value);
+
+    //price calcolations
+    if (!isNaN (km)){                //verification if km is a number
+        price = km * kmPrice         //full price
+    } else{
+        alert('ops... qualcosa è andato storto prova ad inserire un numero nei campi richiesti')
+    }
+
+    //discounted prices
+    if (!isNaN(age)){                                           //verification if age is a number
+        if (age < underAge){                                    //discount under 18
+            price = price - ((price * underDiscount) / 100);
+        } else if (age > overAge){                              //discount over 65
+            price = price - ((price * overDiscount) / 100);
+        }
+        console.log(`€ ${ price.toFixed(2)}`);
+    } else{
+        
+    }
     
     //clear input areas
     kmInput.value = '' ;
